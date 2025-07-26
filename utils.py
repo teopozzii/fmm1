75a5cf3017791483355040e9f24ca40e9c30e441
Matteo Pozzi <matteopozzi@MacBook-Air-di-Matteo-5.local>
2025-07-26 22:26:52 +0200

    Streamline the code, including utils function to fetch data from AV

--- utils.py
+++ utils.py
@@ -1,19 +1,34 @@
 import time
 import requests
-import numpy as np
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 API_KEY = os.getenv('ALPHA_VANTAGE_API_KEY')  # Ensure the API key is loaded from .env
 
-def get_current_fundamentals(symbol):
-    url = f"https://www.alphavantage.co/query?function=OVERVIEW&symbol={symbol}&apikey={API_KEY}"
+def get_fundamentals(ticker, function='o'):
+    """
+    Fetches the current fundamentals for a given ticker.
+    Returns a dictionary with relevant financial metrics.
+    """
+
+    if function not in ['o', 'c', 'b', 'i']:
+        raise ValueError("Function must be one of 'o' (overview), 'c' (cash flow), 'b' (balance sheet), or 'i' (income statement).")
+    function_map = {
+        'o': 'OVERVIEW',
+        'c': 'CASH_FLOW',
+        'b': 'BALANCE_SHEET',
+        'i': 'INCOME_STATEMENT'
+    }
+    function = function_map[function]
+
+    function = function.upper()
+    url = f"https://www.alphavantage.co/query?function={function}&symbol={ticker}&apikey={API_KEY}"
     r = requests.get(url)
     data = r.json()
-    time.sleep(5) # To avoid hitting the API rate limit; needed? boh
-    
-    out = {}
-    for key in data:
-        out[key] = data[key] if data[key] else np.nan
-    return out
+    time.sleep(5) # To avoid hitting the API rate limit; needed?
+
+    if function != 'OVERVIEW' and 'annualReports' in data:
+        return data['annualReports']
+    else:
+        return data
